4bebb7c6c3d68536e5de405e1c64da688dbef079
Alexei <1996Leha@mail.ru>
2022-02-12 00:06:25 +0500

    Исправление высоты карточек

--- dist/js/script.js
+++ dist/js/script.js
@@ -75,16 +75,16 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": function() { return /* binding */ clickCard; }
 /* harmony export */ });
 function clickCard() {
-  let cards = document.querySelector('.cards__wrapper');
+  let cards = document.querySelector('.row_cards');
   cards.addEventListener('click', event => {
     if (event.target.getAttribute('data-id') || event.target.tagName == 'A') {
       let cardTarget = event.target.closest('.card');
       let category = cardTarget.querySelector('.card__category'),
           author = cardTarget.querySelector('.card__author'),
           btn = cardTarget.querySelector('.btn');
-      category.classList.add('card__category_blue');
-      author.classList.add('card__author_blue');
-      btn.classList.add('btn_blue');
+      category.classList.toggle('card__category_blue');
+      author.classList.toggle('card__author_blue');
+      btn.classList.toggle('btn_blue');
     }
   });
 }
--- src/js/modules/clickCard.js
+++ src/js/modules/clickCard.js
@@ -1,5 +1,5 @@
 export default function clickCard () {
-    let cards = document.querySelector('.cards__wrapper');
+    let cards = document.querySelector('.row_cards');
 
     cards.addEventListener('click', (event) => {
         if(event.target.getAttribute('data-id') || event.target.tagName == 'A') {
@@ -9,9 +9,9 @@ export default function clickCard () {
                 author = cardTarget.querySelector('.card__author'),
                 btn = cardTarget.querySelector('.btn');
             
-            category.classList.add('card__category_blue')
-            author.classList.add('card__author_blue')
-            btn.classList.add('btn_blue')
+            category.classList.toggle ('card__category_blue')
+            author.classList.toggle ('card__author_blue')
+            btn.classList.toggle ('btn_blue')
         }
     })
 }
